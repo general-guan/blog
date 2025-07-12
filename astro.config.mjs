22ab594f4f5b686b0106aca8442519ac9d1d9fa5
guanjunjiang <934750771@qq.com>
2025-07-12 22:24:32 +0800

    array

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,36 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "My Docs",
+      locales: {
+        root: {
+          label: "简体中文",
+          lang: "zh-CN",
+        },
+      },
+      social: [
+        {
+          icon: "github",
+          label: "GitHub",
+          href: "https://github.com/general-guan/blog",
+        },
+      ],
+      sidebar: [
+        {
+          label: "JavaScript",
+          items: [
+            {
+              label: "标准库",
+              items: [{ label: "Array 对象", slug: "javascript/stdlib/array" }],
+            },
+          ],
+        },
+      ],
+    }),
+  ],
 });
